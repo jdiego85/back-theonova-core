ce5cc443e4e3313296af7d11e31a8e2baac24f15
juandiego85 <digisoundol@hotmail.com>
2025-12-13 18:01:46 -0500

    first commit

---
+++ src/main/java/com/theonova/BackTheonovaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.theonova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTheonovaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTheonovaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theonova/BackTheonovaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theonova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTheonovaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
