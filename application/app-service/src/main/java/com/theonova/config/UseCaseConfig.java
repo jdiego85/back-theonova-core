95a83d5de732d5eb656a9c8b12aa00f4a50fbfbd
juandiego85 <digisoundol@hotmail.com>
2025-12-21 13:03:45 -0500

    the base project configuration is complete

---
+++ application/app-service/src/main/java/com/theonova/config/UseCaseConfig.java
@@ -0,0 +1,9 @@
+package com.theonova.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.theonova")
+public class UseCaseConfig {
+}
